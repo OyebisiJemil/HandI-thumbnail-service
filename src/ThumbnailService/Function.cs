328578367a1aa583e048736847edc068a2b11257
Jemil <oyebisijemil@gmail.com>
2020-10-07 07:53:18 -0400

    Added functionality for thumnail generation Lambda function receives ObjectCreated event from the source S3 bucket, generates the thumnail and upload the image to the destination S3 bucket

--- src/ThumbnailService/Function.cs
+++ src/ThumbnailService/Function.cs
@@ -1,12 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
-using Amazon.S3.Util;
+using Amazon.S3.Model;
+using ImageMagick;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -46,22 +45,70 @@ namespace ThumbnailService
         public async Task<string> FunctionHandler(S3Event evnt, ILambdaContext context)
         {
             var s3Event = evnt.Records?[0].S3;
-            if(s3Event == null)
+            string thumnailResultBucket = "thumbnail-result-s3-bucket";
+            if (s3Event == null)
             {
                 return null;
             }
 
             try
             {
-                var response = await this.S3Client.GetObjectMetadataAsync(s3Event.Bucket.Name, s3Event.Object.Key);
-                return response.Headers.ContentType;
+                var thumbnailStream =await GenerateThumbnail(s3Event.Bucket.Name, s3Event.Object.Key, context);
+                var thumbnailKey =await UploadThumbnail(thumnailResultBucket, s3Event.Object.Key, thumbnailStream);
+                string res = $"Thumbnail saved to s3://{thumnailResultBucket}/{thumbnailKey}";
+                context.Logger.LogLine(res);
+                return res;
+
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 context.Logger.LogLine($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
                 context.Logger.LogLine(e.Message);
                 context.Logger.LogLine(e.StackTrace);
                 throw;
+            }
+        }
+
+        private async Task<string> UploadThumbnail(string bucketName,string key, MemoryStream thumbnailImageStream)
+        {
+            var index = key.LastIndexOf('/');
+            var thumbnailKey = "thumbnails/" + (index != -1 ? key.Substring(index + 1) : key);
+            await this.S3Client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = thumbnailKey,
+                InputStream = thumbnailImageStream
+            });
+
+            return thumbnailKey;
+        }
+
+        private async Task<MemoryStream> GenerateThumbnail(string bucketName, string key,ILambdaContext context)
+        {
+
+            MemoryStream resizedImageStream;
+            using (var response = await this.S3Client.GetObjectAsync(bucketName, key))
+            {
+                context.Logger.Log("Magick Image Resize");
+                try
+                {
+                    MagickImage magickImage = new MagickImage();
+                    using (MagickImage image = new MagickImage(response.ResponseStream))
+                    {
+                        image.Resize(200, 200);
+                        context.Logger.LogLine($"Image resized");
+
+                        resizedImageStream = new MemoryStream();
+                        image.Write(resizedImageStream);
+                        resizedImageStream.Position = 0;
+                        return resizedImageStream;
+                    }
+                }
+                catch (Exception e)
+                {
+                    context.Logger.Log(e.Message);
+                    throw;
+                }
             }
         }
     }
